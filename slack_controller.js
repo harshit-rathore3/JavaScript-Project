d471892204801739c5d4b7a41d62248e52d70354
Harshit Rathore <harshit.rathore@techolution.com>
2024-04-28 20:33:30 +0530

    Refactored code updated

--- slack_controller.js
+++ slack_controller.js
@@ -1,37 +1,35 @@
+ 
 // @ts-check
+import logger from "../../logger";
+import axios from "axios";
 
-const logger = require("../../logger");
-const axios = require("axios");
+/**
+ * @typedef {{
+ *   slackApiUrl: string,
+ *   slackRequestBody: Object
+ * }} RequestBody
+ */
 
-exports.slackGenericPostApi = async (req, res, next) => {
-    try {
-        /**
-         * @typedef {{
-         *  slackApiUrl: string,
-         *  slackRequestBody: Object
-         * }} RequestBody
-         */
+/**
+ * @param {import('express').Request<{}, {}, RequestBody>} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const slackGenericPostApi = async (req, res, next) => {
+  try {
+    const reqBody = req.body;
+    logger.info(`slackGenericPostApi ~ reqBody ${JSON.stringify(reqBody)}`);
 
-        /** @type {RequestBody} */
-        const reqBody = req.body;
-        logger.info(
-            "exports.slackGenericPostApi= ~ reqBody " + JSON.stringify(reqBody)
-        );
-
-        // @ts-ignore
-        axios
-            .post(reqBody.slackApiUrl, { ...reqBody.slackRequestBody })
-            .then((apiResponse) => {
-                res.json(apiResponse);
-            })
-            .catch((apiResponse) => {
-                logger.error(
-                    "exports.slackGenericPostApi= ~ apiResponse " +
-                        apiResponse.message
-                );
-                res.status(200).json(apiResponse.data);
-            });
-    } catch (error) {
-        next(error);
-    }
+    await axios
+      .post(reqBody.slackApiUrl, { ...reqBody.slackRequestBody })
+      .then((apiResponse) => {
+        res.json(apiResponse);
+      })
+      .catch((apiResponse) => {
+        logger.error(`slackGenericPostApi ~ apiResponse ${apiResponse.message}`);
+        res.status(200).json(apiResponse.data);
+      });
+  } catch (error) {
+    next(error);
+  }
 };
