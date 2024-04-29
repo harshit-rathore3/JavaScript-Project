2019de9d3cf35b975cfc5fd6e11113f5e1ea3a90
Harshit Rathore <harshit.rathore@techolution.com>
2024-04-29 17:24:30 +0530

    Refactored code updated

--- csv.controller.js
+++ csv.controller.js
@@ -1,38 +1,31 @@
-const db = require("../models");
-const Model = db.model;
-const Resource = db.resource;
 
+const db = require("../models");
+const { model: Model, resource: Resource } = db;
 const writeToCSVFile = require("../middlewares/csvOperator");
 
 exports.downloadCSV = async (req, res, next) => {
-    try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return res.status(400).json({
-                    status: 400,
-                    message: "Bad Request",
-                    errors: errors.array(),
-                });
-            }
-    
-            const type = req.body.type;
-            const ids = req.body.image_data;
-    
-            const result = await Model.findOne(
-                { name: type },
-                { _id: 0, headers: 1 }
-            );
-            const headers = [result.headers];
-    
-            const csvs = await Resource.find(
-                { _id: { $in: ids } },
-                { _id: 0, csv: 1 }
-            );
-            const rows = csvs.map((record) => record.csv);
-    
-            const filepath = await writeToCSVFile(headers, rows);
-            res.status(200).send({ filepath: filepath });
-    } catch (error) {
-        next(error);
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        status: 400,
+        message: "Bad Request",
+        errors: errors.array(),
+      });
     }
+
+    const { type } = req.body;
+    const { image_data: ids } = req.body;
+
+    const modelResult = await Model.findOne({ name: type }, { _id: 0, headers: 1 });
+    const headers = [modelResult.headers];
+
+    const csvRecords = await Resource.find({ _id: { $in: ids } }, { _id: 0, csv: 1 });
+    const rows = csvRecords.map((record) => record.csv);
+
+    const filePath = await writeToCSVFile(headers, rows);
+    res.status(200).send({ filePath });
+  } catch (error) {
+    next(error);
+  }
 };
