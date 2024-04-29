4e89c5024e6d18c028444de49600968366b11132
Harshit Rathore <harshit.rathore@techolution.com>
2024-04-29 19:01:27 +0530

    Refactored code updated

--- destructionTest.js
+++ destructionTest.js
@@ -1,37 +1,32 @@
+
 function createUser(req, res) {
-    const name = req.body.name;
-    const email = req.body.email;
-    const age = req.bo.age;
+  const { name, email, age } = req.body;
+  // Create a new user with the provided data
+  // ...
+}
 
-  }
+function getUserAddress(user) {
+  const { street, city, country } = user.address;
+  // Return the user's address details
+  return { street, city, country };
+}
 
+function getUserProperty(user, propertyName) {
+  // Return the value of the specified property for the given user
+  return user[propertyName];
+}
 
-  function getUserAddress(user) {
-    const street = user.address.street;
-    const city = user.address.city;
-    const country = user.address.country;
+function getProductDetails(product) {
+  const name = product.name || 'Unknown';
+  const price = product.price || 0;
+  const category = product.category || 'Uncategorized';
+  // Return the product details
+  return { name, price, category };
+}
 
-  }
-//errors,return
-  functin getUserProperty(user, propertyName) {
-    retur user[propertyName];
-  }
-
-
-
-  
-  function getProductDetails(product) {
-    const name = prod.name || 'Unknown';
-    const price = product.price || 0;
-    const category = product.cat || 'Uncategorized';
-
-  }
-
-  function logUserDetails(user) {
-    console.log('Name:', user.name);
-    console.log('Email:', user.email);
-    console.log('Other details:', {
-      age: user.age,
-      address: user.address,
-    });
-  }
+function logUserDetails(user) {
+  console.log('Name:', user.name);
+  console.log('Email:', user.email);
+  console.log('Other details:', { age: user.age, address: user.address });
+  // Log the user's details to the console
+}
